3d301dfc3a44a6b2a1cf92bdfb451faf9a38d2e4
Kirk Byers <ktbyers@twb-tech.com>
2019-11-13 12:33:01 -0800

    day2 recap exercises

--- day2/recap/exercise2.md
+++ day2/recap/exercise2.md
@@ -1,3 +1,3 @@
 # Day 2 Recap Exercise 2
 
-1. Complete the same tasks as in exercise 1, however this time use Netmiko to capture the "show ip int brief" output from the cisco1 device.
+1. Complete the same tasks as in exercise 1, however this time use Netmiko to capture the "show ip int brief" output from the cisco3 device.
--- day2/recap/solutions/exercise2.py
+++ day2/recap/solutions/exercise2.py
@@ -4,21 +4,25 @@ from pprint import pprint
 
 from netmiko import ConnectHandler
 
-cisco1 = {
+device = {
     "device_type": "cisco_ios",
-    "host": "cisco1.lasthop.io",
+    "host": "cisco3.lasthop.io",
     "username": "pyclass",
     "password": getpass(),
 }
-conn = ConnectHandler(**cisco1)
+conn = ConnectHandler(**device)
 output = conn.send_command("show ip int brief")
 
 ip_dict = {}
-for line in output:
+for line in output.splitlines():
     line = line.strip()
     if "Interface" in line:
         continue
-    interface, ip_address, _, _, status, protocol = line.split()
+    elif "administratively down" in line:
+        interface, ip_address, _, _, status1, status2, protocol = line.split()
+        status = f"{status1}_{status2}"
+    else:
+        interface, ip_address, _, _, status, protocol = line.split()
     fields = {"ip_address": ip_address, "line_status": status, "line_protocol": protocol}
     ip_dict[interface] = fields
 
